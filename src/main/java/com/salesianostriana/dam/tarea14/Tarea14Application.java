7adccda78c325dea8c3ce4796663e86f31867e27
mauroz9 <mauro.serranoo26@gmail.com>
2025-11-14 09:07:01 +0100

    creacion del proyecto

---
+++ src/main/java/com/salesianostriana/dam/tarea14/Tarea14Application.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.tarea14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea14Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/salesianostriana/dam/tarea14/Tarea14ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.tarea14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea14ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
